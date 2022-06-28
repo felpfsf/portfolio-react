324b7b55e4fd2ba19c0e79ff99c670d59d2fedf7
felpfsf <feferreira.sf@gmail.com>
2022-06-28 20:39:16 -0300

    ajustes responsividade e menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,55 @@
 export function Header() {
   return (
-    <nav className="sm:p-4 sm:mx-1 sm:mb-8 border-palette1-alabaster flex items-center md:justify-between sm:justify-center">
+    <nav className="sm:p-4 lg:px-36 sm:mx-8 md:mx-4 lg:mx-4 mb-8 border-b border-palette1-alabaster flex items-center sm:justify-center md:justify-between">
       <h1 className="font-cursive sm:text-4xl text-palette1-sunray">{'< '}Felipe F.{' />'}</h1>
       <div>
-        <ul role="list" className="font-barlowCondensed text-palette1-sunray text-xl md:flex md:gap-2 sm:hidden">
-          <li><a href="#hero" className="hover:text-palette1-platinum transition-colors">Home</a></li>
-          <li><a href="#about" className="hover:text-palette1-platinum transition-colors">Sobre</a></li>
-          <li><a href="#skills" className="hover:text-palette1-platinum transition-colors">Skills</a></li>
-          <li><a href="#schollarship" className="hover:text-palette1-platinum transition-colors">Formação</a></li>
-          <li><a href="#projects" className="hover:text-palette1-platinum transition-colors">Projetos</a></li>
-          <li><a href="#contact" className="hover:text-palette1-platinum transition-colors">Contato</a></li>
+        <ul role="list" className="font-barlowCondensed text-palette1-sunray text-xl md:flex md:gap-4 lg:gap-8 sm:hidden">
+          <li>
+            <a
+              href="#hero"
+              className="hover:text-palette1-platinum hover:underline hover:underline-offset-8 transition-all"
+            >Home
+            </a>
+          </li>
+          <li>
+            <a
+              href="#about"
+              className="hover:text-palette1-platinum hover:underline hover:underline-offset-8 transition-all"
+            >Sobre
+            </a>
+          </li>
+          <li>
+            <a
+              href="#skills"
+              className="hover:text-palette1-platinum hover:underline hover:underline-offset-8 transition-all"
+            >
+              Skills
+            </a>
+          </li>
+          <li>
+            <a
+              href="#schollarship"
+              className="hover:text-palette1-platinum hover:underline hover:underline-offset-8 transition-all"
+            >
+              Formação
+            </a>
+          </li>
+          <li>
+            <a
+              href="#projects"
+              className="hover:text-palette1-platinum hover:underline hover:underline-offset-8 transition-all"
+            >
+              Projetos
+            </a>
+          </li>
+          <li>
+            <a
+              href="#contact"
+              className="hover:text-palette1-platinum hover:underline hover:underline-offset-8 transition-all"
+            >
+              Contato
+            </a>
+          </li>
         </ul>
       </div>
     </nav>
