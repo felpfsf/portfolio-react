f6ba8c2ce2ac82fd8b23bf75bd000b0f3289812f
felpfsf <feferreira.sf@gmail.com>
2022-06-29 16:22:13 -0300

    seção de habilidades e hobbies

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,56 @@
 export function Skills() {
   return (
     <section id="skills" className="sm:p-4 lg:p-16">
-      <h2 className="text-xl text-palette1-platinum font-bold">Skills {'&'} Hobbies</h2>
+      <h2 className="sm:text-xl lg:text-3xl text-palette1-platinum font-bold text-center mb-8">Skills {'&'} Hobbies</h2>
+
+      <div className="flex flex-row flex-wrap items-center justify-center gap-4">
+
+        <div className="bg-palette1-platinum p-4 flex flex-col items-center justify-center w-32 h-32">
+          <img src="https://via.placeholder.com/752x456.png?text=Imagem_do_projeto" alt="" className="w-20 h-20" />
+          <p className="text-base text-palette1-grayBlue">html</p>
+        </div>
+
+        <div className="bg-palette1-platinum p-4 flex flex-col items-center justify-center w-32 h-32">
+          <img src="https://via.placeholder.com/752x456.png?text=Imagem_do_projeto" alt="" className="w-20 h-20" />
+          <p className="text-base text-palette1-grayBlue">css</p>
+        </div>
+
+        <div className="bg-palette1-platinum p-4 flex flex-col items-center justify-center w-32 h-32">
+          <img src="https://via.placeholder.com/752x456.png?text=Imagem_do_projeto" alt="" className="w-20 h-20" />
+          <p className="text-base text-palette1-grayBlue">javascript</p>
+        </div>
+
+        <div className="bg-palette1-platinum p-4 flex flex-col items-center justify-center w-32 h-32">
+          <img src="https://via.placeholder.com/752x456.png?text=Imagem_do_projeto" alt="" className="w-20 h-20" />
+          <p className="text-base text-palette1-grayBlue">react</p>
+        </div>
+
+      </div>
+
+      <h2 className="sm:text-xl lg:text-3xl text-palette1-platinum font-bold text-center mt-20 mb-8">Hobbies</h2>
+      <div className="flex flex-row flex-wrap items-center justify-center gap-4">
+
+        <div className="bg-palette1-platinum p-4 flex flex-col items-center justify-center w-32 h-32">
+          <img src="https://via.placeholder.com/752x456.png?text=Imagem_do_projeto" alt="" className="w-20 h-20" />
+          <p className="text-base text-palette1-grayBlue">séries de TV</p>
+        </div>
+
+        <div className="bg-palette1-platinum p-4 flex flex-col items-center justify-center w-32 h-32">
+          <img src="https://via.placeholder.com/752x456.png?text=Imagem_do_projeto" alt="" className="w-20 h-20" />
+          <p className="text-base text-palette1-grayBlue">cozinhar</p>
+        </div>
+
+        <div className="bg-palette1-platinum p-4 flex flex-col items-center justify-center w-32 h-32">
+          <img src="https://via.placeholder.com/752x456.png?text=Imagem_do_projeto" alt="" className="w-20 h-20" />
+          <p className="text-base text-palette1-grayBlue">esportes</p>
+        </div>
+
+        <div className="bg-palette1-platinum p-4 flex flex-col items-center justify-center w-32 h-32">
+          <img src="https://via.placeholder.com/752x456.png?text=Imagem_do_projeto" alt="" className="w-20 h-20" />
+          <p className="text-base text-palette1-grayBlue">jogos</p>
+        </div>
+
+      </div>
     </section>
   )
 }
