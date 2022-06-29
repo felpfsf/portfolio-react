3e5a7f1f9896de29f041c10b75b7352b49d9e59c
felpfsf <feferreira.sf@gmail.com>
2022-06-28 21:28:10 -0300

    correções nos estilos

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,25 +4,29 @@ export function Contact() {
       <h2 className="text-xl text-palette1-onyx font-bold mb-2">
         Contato
       </h2>
-      <p className="sm:text-sm md:text-base text-palette1-grayBlue mb-8">
-        Quer entrar em contato comigo?<br />
-        Preencha o formulário e entrarei em contato.
-      </p>
-      <form action="" className="">
-        <input id="name" type="text" placeholder="Digite seu nome" required className="peer input" />
-        <label for="name" className="label">Nome</label>
 
-        <input id="email" type="email" placeholder="Digite seu email" required className="peer input" />
-        <label for="email" className="label">E-mail</label>
+      <div>
+        <p className="sm:text-sm md:text-base text-palette1-grayBlue mb-8">
+          Quer entrar em contato comigo?<br />
+          Preencha o formulário e entrarei em contato.
+        </p>
+        <form action="" className="">
+          <input id="name" type="text" placeholder="Digite seu nome" required className="peer input" />
+          <label for="name" className="label">Nome</label>
 
-        <input id="subject" type="text" placeholder="Assunto" required className="peer input" />
-        <label for="subject" className="label">Assunto da mensagem</label>
+          <input id="email" type="email" placeholder="Digite seu email" required className="peer input" />
+          <label for="email" className="label">E-mail</label>
 
-        <textarea id="message" placeholder="Sua mensagem" required className="textarea" />
-        <label for="message" className="label">Sua Mensagem</label>
+          <input id="subject" type="text" placeholder="Assunto" required className="peer input" />
+          <label for="subject" className="label">Assunto da mensagem</label>
 
-        <button type="submit" className="text-sm text-palette1-platinum bg-palette1-sunray py-3 px-4 w-[150px]">Enviar mensagem</button>
-      </form>
+          <textarea id="message" placeholder="Sua mensagem" required className="textarea" />
+          <label for="message" className="label">Sua Mensagem</label>
+
+          <button type="submit" className="text-sm text-palette1-platinum bg-palette1-sunray py-3 px-4 w-[150px]">Enviar mensagem</button>
+        </form>
+      </div>
+
     </section>
   )
 }
