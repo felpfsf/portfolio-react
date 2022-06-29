5778c6d9f47126ddfbce6f5943d4e10cec68a3af
felpfsf <feferreira.sf@gmail.com>
2022-06-29 20:52:26 -0300

    alteração form de contato resolução desktop

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,18 @@
 export function Contact() {
   return (
-    <section id="contact" className="bg-palette1-platinum sm:p-4 lg:p-16">
-      <h2 className="text-xl text-palette1-onyx font-bold mb-2">
-        Contato
-      </h2>
+    <section id="contact" className="bg-palette1-platinum sm:p-4 lg:p-16 lg:flex lg:justify-center lg:gap-20">
+      <img src="https://via.placeholder.com/464x593.png?text=Imagem_do_projeto" alt="" className/>
+      <div className="w-1/3">
+        <div>
+          <h2 className="text-xl text-palette1-onyx font-bold mb-2">
+            Contato
+          </h2>
 
-      <div>
-        <p className="sm:text-sm md:text-base text-palette1-grayBlue mb-8">
-          Quer entrar em contato comigo?<br />
-          Preencha o formulário e entrarei em contato.
-        </p>
+          <p className="sm:text-sm md:text-base text-palette1-grayBlue mb-8">
+            Quer entrar em contato comigo?<br />
+            Preencha o formulário e entrarei em contato.
+          </p>
+        </div>
         <form action="" className="">
           <input id="name" type="text" placeholder="Digite seu nome" required className="peer input" />
           <label htmlFor="name" className="label">Nome</label>
@@ -26,7 +29,6 @@ export function Contact() {
           <button type="submit" className="text-sm text-palette1-platinum bg-palette1-sunray py-3 px-4 w-[150px] primary:bg-palette1-fireOpal transition-colors">Enviar mensagem</button>
         </form>
       </div>
-
     </section>
   )
 }
