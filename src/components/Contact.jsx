ccc42fcdf2044c3b640440eb81ce5f09b0fd28a5
felpfsf <feferreira.sf@gmail.com>
2022-06-30 12:53:19 -0300

    correções na exibição do formulário

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,8 @@
 export function Contact() {
   return (
     <section id="contact" className="bg-palette1-platinum sm:p-4 lg:p-16 lg:flex lg:justify-center lg:gap-20">
-      <img src="https://via.placeholder.com/464x593.png?text=Imagem_do_projeto" alt="" className/>
-      <div className="w-1/3">
+      <img src="https://via.placeholder.com/464x593.png?text=Imagem_do_projeto" alt="" className="sm:hidden md:hidden lg:block" />
+      <div className="lg:w-1/3 sm:w-full">
         <div>
           <h2 className="text-xl text-palette1-onyx font-bold mb-2">
             Contato
