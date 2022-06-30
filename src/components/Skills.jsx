9754c3c12aa4d9d06a853130258532140591b9a9
felpfsf <feferreira.sf@gmail.com>
2022-06-30 17:25:38 -0300

    correção de css

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,9 +4,6 @@ import { SkillsCards } from "./SkillsCards"
 
 
 export function Skills() {
-  // const skillCardElem = skills.map(skillData => {
-
-  // })
   return (
     <section id="skills" className="sm:p-4 lg:p-16">
       <h2 className="sm:text-xl lg:text-3xl text-palette1-platinum font-bold text-center mb-8">Skills {'&'} Hobbies</h2>
@@ -44,25 +41,25 @@ export function Skills() {
       <h2 className="sm:text-xl lg:text-3xl text-palette1-platinum font-bold text-center mt-20 mb-8">Hobbies</h2>
       <div className="flex flex-row flex-wrap items-center justify-center gap-4">
 
-        <div className=" p-4 border-r flex flex-col items-center justify-center w-32 h-32">
+        <div className=" p-4 flex flex-col items-center justify-center w-32 h-32">
           {/* <img src="https://via.placeholder.com/752x456.png?text=Imagem_do_projeto" alt="" className="w-20 h-20" /> */}
           <TelevisionSimple size={56} className="text-palette1-alabaster font-bold" />
           <p className="text-base text-palette1-alabaster font-bold mt-2">Séries TV</p>
         </div>
 
-        <div className=" p-4 border-r flex flex-col items-center justify-center w-32 h-32">
+        <div className=" p-4 flex flex-col items-center justify-center w-32 h-32">
           {/* <img src="https://via.placeholder.com/752x456.png?text=Imagem_do_projeto" alt="" className="w-20 h-20" /> */}
           <CookingPot size={56} className="text-palette1-alabaster font-bold" />
           <p className="text-base text-palette1-alabaster font-bold mt-2">Cozinhar</p>
         </div>
 
-        <div className=" p-4 border-r flex flex-col items-center justify-center w-32 h-32">
+        <div className=" p-4 flex flex-col items-center justify-center w-32 h-32">
           {/* <img src="https://via.placeholder.com/752x456.png?text=Imagem_do_projeto" alt="" className="w-20 h-20" /> */}
           <PersonSimpleRun size={56} className="text-palette1-alabaster font-bold" />
           <p className="text-base text-palette1-alabaster font-bold mt-2">Esportes</p>
         </div>
 
-        <div className=" p-4 border-r flex flex-col items-center justify-center w-32 h-32">
+        <div className=" p-4 flex flex-col items-center justify-center w-32 h-32">
           {/* <img src="https://via.placeholder.com/752x456.png?text=Imagem_do_projeto" alt="" className="w-20 h-20" /> */}
           <GameController size={56} className="text-palette1-alabaster font-bold" />
           <p className="text-base text-palette1-alabaster font-bold mt-2">Jogos</p>
--- src/components/SkillsCards.jsx
+++ src/components/SkillsCards.jsx
@@ -1,7 +1,7 @@
 
 export function SkillsCards(props) {
   return (
-    <div className=" p-4 border-r flex flex-col items-center justify-center w-32 h-32">
+    <div className=" p-4 flex flex-col items-center justify-center w-32 h-32">
       <img src={props.imgUrl} alt="" className="w-20 h-20 text-palette1-onyx" />
       <p className="text-base text-palette1-alabaster mt-2 font-bold">{props.title}</p>
     </div>
